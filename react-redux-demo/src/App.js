d22e86f260f607faa8495e876825f7d3b4a0fa7c
faithOnly <firdevssyildirimm@gmail.com>
2020-09-30 13:06:39 -0500

    Component created with hooks

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -4,12 +4,14 @@ import store from './redux/store'
 
 import './App.css';
 import CakeContainer from './Components/Cakecontainer';
+import HooksCakeContainer from './Components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store ={store} >
     <div className="App">
       <CakeContainer />
+      <HooksCakeContainer />
     </div>
     </Provider>
   );
